983bb627e39eca998247232fee057e0d9e403404
Mizu <psdk55052004@gmail.com>
2023-11-20 13:32:53 +0800

    建立環境

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+@import "src/assets/styleSheets/all.scss";
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <template v-for="item in data" :key="item.id">
+    <component :is="item.component"></component>
+  </template>
+</template>
+
+<script setup>
+import { defineAsyncComponent, reactive } from 'vue'
+
+const data = reactive([{
+  id: 1,
+  name: 'Demo1',
+  component: ''
+}])
+
+getComponents()
+
+function getComponents () {
+  data.forEach((item) => {
+    item.component = defineAsyncComponent(() => import(`../components/${item.name}.vue`))
+  })
+}
+</script>
+
+<style lang='scss' scope></style>
