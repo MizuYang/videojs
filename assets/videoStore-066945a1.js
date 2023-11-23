be49e00d71126d28d405fb35a0e1658d435b165f
Mizu <psdk55052004@gmail.com>
2023-11-23 16:37:14 +0800

    deploy

--- assets/videoStore-066945a1.js
+++
@@ -1,1 +0,0 @@
-import{l as e,i as r}from"./index-2c2aff26.js";const s=e("videoStore",()=>({player:r(null)}));export{s as u};
---
+++ assets/videoStore-4f1f1215.js
@@ -0,0 +1,1 @@
+import{l as e,i as r}from"./index-9397b9fa.js";const s=e("videoStore",()=>({player:r(null)}));export{s as u};
