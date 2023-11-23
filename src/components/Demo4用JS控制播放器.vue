f3fbc6176d3d1cfe15b96b1712e2f33e409c9104
Mizu <psdk55052004@gmail.com>
2023-11-23 16:25:04 +0800

    測試 修改字幕檔的路徑

--- src/components/Demo4用JS控制播放器.vue
+++ src/components/Demo4用JS控制播放器.vue
@@ -7,7 +7,8 @@
       <video id="video3"
              class="video-js">
         <source src="@/assets/files/影片1.mp4" type="video/mp4">
-        <track src="../../public/字幕.vtt" kind="subtitles" label="中文字幕" srclang="zh" default>
+        <track src="/字幕.vtt" kind="subtitles" label="中文字幕" srclang="zh" default>
+        <!-- <track src="../../public/字幕.vtt" kind="subtitles" label="中文字幕" srclang="zh" default> -->
       </video>
     </section>
 
